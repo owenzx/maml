6c894faa2c7f6ecc6c147b0274b364905b41862e
owenzx <owen9608@outlook.com>
2018-09-24 00:34:52 -0400

    lets see

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ from maml import MAML
 from tensorflow.python.platform import flags
 import os
 from datetime import datetime
+import threading
 
 FLAGS = flags.FLAGS
 
@@ -74,99 +75,32 @@ flags.DEFINE_integer('gpu_id', -1, 'the id of the gpu to use')
 flags.DEFINE_bool('fast', True, "whether to use python for loop instead of tf.map_fn")
 
 def train(model, saver, sess, exp_string, data_generator, resume_itr=0):
-    SUMMARY_INTERVAL = 100
-    SAVE_INTERVAL = 1000
-    if FLAGS.datasource == 'sinusoid':
-        PRINT_INTERVAL = 1000
-        TEST_PRINT_INTERVAL = PRINT_INTERVAL*5
-    else:
-        PRINT_INTERVAL = 100
-        TEST_PRINT_INTERVAL = PRINT_INTERVAL*5
+    batch_x, batch_y, amp, phase = data_generator.generate()
 
-    if FLAGS.log:
-        train_writer = tf.summary.FileWriter(FLAGS.logdir + '/' + exp_string, sess.graph)
-    print('Done initializing, starting training.')
-    prelosses, postlosses = [], []
+    if FLAGS.baseline == 'oracle':
+        batch_x = np.concatenate([batch_x, np.zeros([batch_x.shape[0], batch_x.shape[1], 2])], 2)
+        for i in range(FLAGS.meta_batch_size):
+            batch_x[i, :, 1] = amp[i]
+            batch_x[i, :, 2] = phase[i]
 
-    num_classes = data_generator.num_classes # for classification, 1 otherwise
-    multitask_weights, reg_weights = [], []
+    inputa = batch_x[:, :num_classes*FLAGS.update_batch_size, :]
+    labela = batch_y[:, :num_classes*FLAGS.update_batch_size, :]
+    inputb = batch_x[:, num_classes*FLAGS.update_batch_size:, :] # b used for testing
+    labelb = batch_y[:, num_classes*FLAGS.update_batch_size:, :]
+    feed_dict = {model.inputa: inputa, model.inputb: inputb,  model.labela: labela, model.labelb: labelb}
 
-    for itr in range(resume_itr, FLAGS.pretrain_iterations + FLAGS.metatrain_iterations):
-        feed_dict = {}
-        if 'generate' in dir(data_generator):
-            batch_x, batch_y, amp, phase = data_generator.generate()
 
-            if FLAGS.baseline == 'oracle':
-                batch_x = np.concatenate([batch_x, np.zeros([batch_x.shape[0], batch_x.shape[1], 2])], 2)
-                for i in range(FLAGS.meta_batch_size):
-                    batch_x[i, :, 1] = amp[i]
-                    batch_x[i, :, 2] = phase[i]
 
-            inputa = batch_x[:, :num_classes*FLAGS.update_batch_size, :]
-            labela = batch_y[:, :num_classes*FLAGS.update_batch_size, :]
-            inputb = batch_x[:, num_classes*FLAGS.update_batch_size:, :] # b used for testing
-            labelb = batch_y[:, num_classes*FLAGS.update_batch_size:, :]
-            feed_dict = {model.inputa: inputa, model.inputb: inputb,  model.labela: labela, model.labelb: labelb}
-
-        if itr < FLAGS.pretrain_iterations:
-            input_tensors = [model.pretrain_op]
-        else:
-            input_tensors = [model.metatrain_op]
-
-        if (itr % SUMMARY_INTERVAL == 0 or itr % PRINT_INTERVAL == 0):
-            input_tensors.extend([model.summ_op, model.total_loss1, model.total_losses2[FLAGS.num_updates-1]])
-            if model.classification:
-                input_tensors.extend([model.total_accuracy1, model.total_accuracies2[FLAGS.num_updates-1]])
-
-        result = sess.run(input_tensors, feed_dict)
-
-        print(str(datetime.now())+" itr%d"%itr)
-
-        if itr % SUMMARY_INTERVAL == 0:
-            prelosses.append(result[-2])
-            if FLAGS.log:
-                train_writer.add_summary(result[1], itr)
-            postlosses.append(result[-1])
-
-        if (itr!=0) and itr % PRINT_INTERVAL == 0:
-            if itr < FLAGS.pretrain_iterations:
-                print_str = 'Pretrain Iteration ' + str(itr)
-            else:
-                print_str = 'Iteration ' + str(itr - FLAGS.pretrain_iterations)
-            print_str += ': ' + str(np.mean(prelosses)) + ', ' + str(np.mean(postlosses))
-            print(print_str)
-            prelosses, postlosses = [], []
-
-        if (itr!=0) and itr % SAVE_INTERVAL == 0:
-            saver.save(sess, FLAGS.logdir + '/' + exp_string + '/model' + str(itr))
-
-        # sinusoid is infinite data, so no need to test on meta-validation set.
-        if (itr!=0) and itr % TEST_PRINT_INTERVAL == 0 and FLAGS.datasource !='sinusoid':
-            if 'generate' not in dir(data_generator):
-                feed_dict = {}
-                if model.classification:
-                    input_tensors = [model.metaval_total_accuracy1, model.metaval_total_accuracies2[FLAGS.num_updates-1], model.summ_op]
-                else:
-                    input_tensors = [model.metaval_total_loss1, model.metaval_total_losses2[FLAGS.num_updates-1], model.summ_op]
-            else:
-                batch_x, batch_y, amp, phase = data_generator.generate(train=False)
-                inputa = batch_x[:, :num_classes*FLAGS.update_batch_size, :]
-                inputb = batch_x[:, num_classes*FLAGS.update_batch_size:, :]
-                labela = batch_y[:, :num_classes*FLAGS.update_batch_size, :]
-                labelb = batch_y[:, num_classes*FLAGS.update_batch_size:, :]
-                feed_dict = {model.inputa: inputa, model.inputb: inputb,  model.labela: labela, model.labelb: labelb, model.meta_lr: 0.0}
-                if model.classification:
-                    input_tensors = [model.total_accuracy1, model.total_accuracies2[FLAGS.num_updates-1]]
-                else:
-                    input_tensors = [model.total_loss1, model.total_losses2[FLAGS.num_updates-1]]
-
-            result = sess.run(input_tensors, feed_dict)
-            print('Validation results: ' + str(result[0]) + ', ' + str(result[1]))
-
-    saver.save(sess, FLAGS.logdir + '/' + exp_string +  '/model' + str(itr))
-
-# calculated for omniglot
-NUM_TEST_POINTS = 600
+    def train_function():
+        while True:
+            sess.run([model.metatrain_op],feed_dict)
+    train_threads = []
+    for _ in range(32):
+        train_threads.append(threading.Thread(target=train_function))
+    for t in train_threads:
+        t.start()
+    for t in train_threads:
+        t.join()
 
 def test(model, saver, sess, exp_string, data_generator, test_num_updates=None):
     num_classes = data_generator.num_classes # for classification, 1 otherwise
